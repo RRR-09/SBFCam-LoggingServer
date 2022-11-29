8f59afdacb6fe020501ff0989736522cf937b722
RRR-09 <79768142+RRR-09@users.noreply.github.com>
2022-11-28 23:55:42 -0500

    Make validation more clear

--- controller.py
+++ controller.py
@@ -1,99 +1,75 @@
 import json
 import traceback
-import uuid
 from typing import List
 
-from models import BufferedData, PlayerData, TemporalData, Vector3
+from models import (
+    BUFFERED_DATA_REQUEST_KEYS_LEN,
+    PLAYER_DATA_REQUEST_KEYS,
+    PLAYER_DATA_REQUEST_KEYS_LEN,
+    TEMPORAL_DATA_REQUEST_KEYS_LEN,
+    BufferedDataRequest,
+    DataArrayLengthMismatch,
+    PlayerDataRequest,
+    TemporalDataRequest,
+)
 
 
-async def format_data(data: List) -> BufferedData:
+async def format_data(data: List) -> BufferedDataRequest:
     """
     Converts positional list-based data to an easier-to-use dict.
 
-    Raises an error if validation mistmatches.
+    Raises an error if validation mismatches.
     """
     # TODO: Fix redundant processing, simplify assertions
-    assert len(data) == 3
+
+    if len(data) != BUFFERED_DATA_REQUEST_KEYS_LEN:
+        raise DataArrayLengthMismatch(
+            model="BufferedDataRequest",
+            expected_len=BUFFERED_DATA_REQUEST_KEYS_LEN,
+            received_len=len(data),
+        )
 
     server_id = data[0]
-    if not server_id:
-        raise ValueError("server_id is empty/null")
-    server_id = uuid.UUID(str(server_id))
-
     server_version = data[1]
-    server_version = int(server_version)
+    timepoints = data[2]
 
-    buffered_data = data[2]
-    assert isinstance(buffered_data, list)
+    processed_buffer_data = []
 
-    formatted_buffered_data = BufferedData(
-        server_id=server_id, server_version=server_version, buffered_data=[]
-    )
-    for timepoint in buffered_data:
-        timestamp = timepoint[0]
-        timestamp = float(timestamp)
-
-        temporal_data = timepoint[1]
-        assert isinstance(timepoint, list)
-
-        formatted_temporal_data = []
-        for player_data in temporal_data:
-            assert len(player_data) == 16
-
-            player_id = player_data[0]
-            player_id = int(player_id)
-
-            player_name = str(player_data[1])
-            assert len(player_name) > 0
-
-            position = {"x": player_data[2], "y": player_data[3], "z": player_data[4]}
-            position["x"] = float(position["x"])
-            position["y"] = float(position["y"])
-            position["z"] = float(position["z"])
-
-            rotation = {"x": player_data[5], "y": player_data[6], "z": player_data[7]}
-            rotation["x"] = float(rotation["x"])
-            rotation["y"] = float(rotation["y"])
-            rotation["z"] = float(rotation["z"])
-
-            size = {"x": player_data[8], "y": player_data[9], "z": player_data[10]}
-            size["x"] = float(size["x"])
-            size["y"] = float(size["y"])
-            size["z"] = float(size["z"])
-
-            velocity = {
-                "x": player_data[11],
-                "y": player_data[12],
-                "z": player_data[13],
-            }
-            velocity["x"] = float(velocity["x"])
-            velocity["y"] = float(velocity["y"])
-            velocity["z"] = float(velocity["z"])
-
-            scaleType = player_data[14]
-            scaleType = str(scaleType)
-            characterType = player_data[15]
-            characterType = str(characterType)
-
-            formatted_temporal_data.append(
-                PlayerData(
-                    player_id=player_id,
-                    player_name=player_name,
-                    position=Vector3.parse_obj(position),
-                    rotation=Vector3.parse_obj(rotation),
-                    size=Vector3.parse_obj(size),
-                    velocity=Vector3.parse_obj(velocity),
-                    scaleType=scaleType,
-                    characterType=characterType,
-                )
+    for timepoint in timepoints:
+        if len(timepoint) != TEMPORAL_DATA_REQUEST_KEYS_LEN:
+            raise DataArrayLengthMismatch(
+                model="TemporalDataRequest",
+                expected_len=TEMPORAL_DATA_REQUEST_KEYS_LEN,
+                received_len=len(timepoint),
             )
 
-        new_timepoint = TemporalData(
-            unix_timestamp=timestamp, player_data=formatted_temporal_data
-        )
-        formatted_buffered_data.buffered_data.append(new_timepoint)
+        unix_timestamp = timepoint[0]
+        temporal_data = timepoint[1]
 
-    return formatted_buffered_data
+        processed_temporal_data: List[PlayerDataRequest] = []
+        for player_data in temporal_data:
+            if len(player_data) != PLAYER_DATA_REQUEST_KEYS_LEN:
+                raise DataArrayLengthMismatch(
+                    model="PlayerDataRequest",
+                    expected_len=PLAYER_DATA_REQUEST_KEYS_LEN,
+                    received_len=len(player_data),
+                )
+
+            processed_player_data = PlayerDataRequest(
+                **dict(zip(PLAYER_DATA_REQUEST_KEYS, player_data))
+            )
+            processed_temporal_data.append(processed_player_data)
+
+        new_timepoint = TemporalDataRequest(
+            unix_timestamp=unix_timestamp, player_data=processed_temporal_data
+        )
+        processed_buffer_data.append(new_timepoint)
+
+    return BufferedDataRequest(
+        server_id=server_id,
+        server_version=server_version,
+        buffered_data=processed_buffer_data,
+    )
 
 
 async def handle_data(data: List):
--- models.py
+++ models.py
@@ -4,39 +4,60 @@ from uuid import UUID
 from pydantic import BaseModel
 
 
-class Vector3(BaseModel):
-    """Basic 3-value vector"""
-
-    x: float
-    y: float
-    z: float
+class DataArrayLengthMismatch(ValueError):
+    def __init__(self, model: str, expected_len: int, received_len: int):
+        message = f"Mismatch in '{str(model)}' (Expected {expected_len}, received {received_len})"
+        super().__init__(message)
 
 
-class PlayerData(BaseModel):
+class PlayerDataRequest(BaseModel):
     """Incoming datapoint, a fragment of data for a specific player"""
 
     player_id: int
     player_name: str
-    position: Vector3
-    rotation: Vector3
-    size: Vector3
-    velocity: Vector3
+    position_x: float
+    position_y: float
+    position_z: float
+    rotation_x: float
+    rotation_y: float
+    rotation_z: float
+    size_x: float
+    size_y: float
+    size_z: float
+    velocity_x: float
+    velocity_y: float
+    velocity_z: float
     scaleType: str
     characterType: str
 
 
-class TemporalData(BaseModel):
+class TemporalDataRequest(BaseModel):
     """Incoming datapoint, a collection of playerdata at a specific point in time"""
 
     unix_timestamp: float
-    player_data: List[PlayerData]
+    player_data: List[PlayerDataRequest]
 
 
-class BufferedData(BaseModel):
+class BufferedDataRequest(BaseModel):
     """Incoming datapoint, effectively the top-level formatted model.
     Server ID and version, containing a list of polled timepoints, each timepoint containing its own collection of
     playerdata collected at that time."""
 
     server_id: UUID
     server_version: int
-    buffered_data: List[TemporalData]
+    buffered_data: List[TemporalDataRequest]
+
+
+# Save values so they're not manually defined/constantly calculated
+PLAYER_DATA_REQUEST_KEYS = list(PlayerDataRequest.__fields__.keys())
+PLAYER_DATA_REQUEST_KEYS_LEN = len(PLAYER_DATA_REQUEST_KEYS)
+TEMPORAL_DATA_REQUEST_KEYS_LEN = len(TemporalDataRequest.__fields__)
+BUFFERED_DATA_REQUEST_KEYS_LEN = len(BufferedDataRequest.__fields__)
+
+
+class Vector3(BaseModel):
+    """Basic 3-value vector"""
+
+    x: float
+    y: float
+    z: float
