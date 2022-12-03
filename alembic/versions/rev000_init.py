4fcda2a672487b7ab50186978bb09e84c2b3218c
RRR-09 <79768142+RRR-09@users.noreply.github.com>
2022-12-03 02:14:14 -0500

    Initial alembic prototype

---
+++ alembic/versions/rev000_init.py
@@ -0,0 +1,20 @@
+"""blank slate, for testing
+
+Revision ID: rev000
+Create Date: 2022-11-29 01:48:11.199554
+
+"""
+
+# revision identifiers, used by Alembic.
+revision = "rev0"
+down_revision = None
+branch_labels = None
+depends_on = None
+
+
+def upgrade() -> None:
+    pass
+
+
+def downgrade() -> None:
+    pass
---
+++ alembic/versions/rev001_create_initial_structure.py
@@ -0,0 +1,47 @@
+"""create initial structure
+
+Revision ID: rev001
+Revises: rev000
+Create Date: 2022-11-29 02:16:34.093120
+
+"""
+import sqlalchemy as sa
+from alembic import op
+
+# revision identifiers, used by Alembic.
+revision = "rev001"
+down_revision = "rev000"
+branch_labels = None
+depends_on = None
+
+
+def upgrade() -> None:
+    op.create_table(
+        "characters",
+        sa.Column("id", sa.Integer, primary_key=True, autoincrement=True),
+        sa.Column("server_version", sa.Integer),
+        sa.Column("timestamp", sa.TIMESTAMP, nullable=False),
+        sa.Column("character_type", sa.String(50), nullable=False),
+        sa.Column("scale_type", sa.String(50), nullable=False),
+        sa.Column("size_x", sa.Float, nullable=False),
+        sa.Column("size_y", sa.Float, nullable=False),
+        sa.Column("size_z", sa.Float, nullable=False),
+    )
+    op.create_table(
+        "users",
+        sa.Column(
+            "user_id", sa.Integer, primary_key=True, nullable=False, autoincrement=False
+        ),
+        sa.Column("last_modified", sa.TIMESTAMP, nullable=False),
+        sa.Column("name", sa.String(50), nullable=True),
+    )
+    op.create_table(
+        "users_history",
+        sa.Column("user_id", sa.Integer, primary_key=True),
+        sa.Column("timestamp", sa.TIMESTAMP, nullable=False),
+        sa.Column("name", sa.String(50), nullable=True),
+    )
+
+
+def downgrade() -> None:
+    pass
